52ff894485e73af8d911bf82e072bffbd31b67f7
Remix Workspace <>
2022-11-04 10:33:38 -0700

    My 2nd Remix commit

---
+++ contracts/Course.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+/**
+ * @title Course contract
+ * @notice A Course contract that holds a buleprint for a course
+ */
+
+contract Course {
+
+    uint256 public name;
+    uint256 public owner;
+
+    Hole[] public hole;
+
+    mapping(string => uint256) public holeToPinPosition;
+
+    struct Hole {
+        string name;
+        uint256 pinPosition;
+        uint256 rating;
+        uint256 holeNumber;
+    }
+
+
+    function addHole(
+        string memory _name,
+        uint256 _pinPosition,
+        uint256 _rating,
+        uint256 _holeNumber
+    ) public{
+        hole.push(Hole(_name, _pinPosition, _rating, _holeNumber));
+        holeToPinPosition[_name] = _pinPosition;
+    }
+
+
+}
